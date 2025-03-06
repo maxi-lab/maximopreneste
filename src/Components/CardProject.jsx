66641e897c029f953aebc78ecca1cac116849a53
Maximo <maxipreneste@gmail.com>
2025-03-06 11:36:40 -0300

    feat(home):project cards

---
+++ src/Components/CardProject.jsx
@@ -0,0 +1,22 @@
+import { Card, CardHeader, Typography,CardMedia, CardContent } from "@mui/material";
+import Grid from '@mui/material/Grid2';
+
+export default function CardProject({title, description, img, link}) {
+    return(<>
+
+        <Grid container spacing={2}>
+            <Card>
+                <CardHeader title={title}/>
+                <CardMedia component="img" alt={`Imagen de ${title}`} image={img} title={title} />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {description}
+                    </Typography>
+                    {link && <a href="" >Ver proyecto</a>}{/*Pagina del proyecto (links dinamicos)*/}
+                </CardContent>
+            </Card>
+        </Grid>
+
+    </>)
+}
+
