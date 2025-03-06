f3232aa6d6008ced7a7ab384c68b6a9d454a5f8b
Maximo <maxipreneste@gmail.com>
2025-03-06 12:15:39 -0300

    feat: create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
 import './App.css'
-import {  Typography } from '@mui/material'
-import Grid from '@mui/material/Grid2';
-import CardProject from './Components/CardProject';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
 
 function App() {
   
 
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid size={{xs: 6, md: 12}}>
-        <Typography variant="h1" align="center">
-        Maximo Preneste
-        </Typography>
-        </Grid>
-        <Grid size={{xs: 12, md: 12}}>
-        <Typography variant='subtitle1' align='center'>
-        Un estudiante de Ingeniería en Sistemas apasionado por el desarrollo web.
-        </Typography>
-        </Grid>
-        <Grid size={{xs: 12, md: 12}}>
-          <CardProject title="Proyecto 1" description="Descripción del proyecto 1" img="https://via.placeholder.com/150" link="https://www.google.com"/>
-        </Grid>
-      </Grid>
+    <Router>
+        <Routes>
+          <Route path="/*" element={<Home />} />
+          <Route path='/proyectos' element={<h1>Proyectos</h1>} />
+        </Routes>
+    </Router>
+      
     </>
   )
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,24 @@
+import Grid from '@mui/material/Grid2';
+import Typography from '@mui/material/Typography';
+import CardProject from '../Components/CardProject';
+export default function Home() {
+    return (
+        <>
+          <Grid container spacing={2}>
+            <Grid size={{xs: 6, md: 12}}>
+            <Typography variant="h1" align="center">
+            Maximo Preneste
+            </Typography>
+            </Grid>
+            <Grid size={{xs: 12, md: 12}}>
+            <Typography variant='subtitle1' align='center'>
+            Un estudiante de Ingeniería en Sistemas apasionado por el desarrollo web.
+            </Typography>
+            </Grid>
+            <Grid size={{xs: 12, md: 12}}>
+              <CardProject title="Proyecto 1" description="Descripción del proyecto 1" img="https://via.placeholder.com/150" link="https://www.google.com"/>
+            </Grid>
+          </Grid>
+        </>
+      )
+}
