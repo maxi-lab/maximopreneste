51c358c3c369f6f8c307d4e8d40f650139db2755
Maximo <maxipreneste@gmail.com>
2025-03-08 20:02:24 -0300

    fix (footer): style margin

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
     return (
         <>
             <footer>
-                <Divider sx={{border:'1px solid'}} />
+                <Divider sx={{border:'1px solid',margin:2}} />
                 <Grid container spacing={2}>
                     <Grid size={{ xs: 12, md: 6 }}>
                         <Typography variant='body2' align='center'>
@@ -20,11 +20,11 @@ export default function Footer() {
                     </Grid>
                     <Grid size={{ xs: 12, md: 6 }}>
                         <Box display='flex' justifyContent='center'>
-                        <Typography variant='h6' align='center'>
+                        <Typography variant='h6' align='center' sx={{margin:'40px',alignContent:'center'}}>
                             Contactame
                         </Typography>
                         <Divider orientation="vertical"variant="middle" flexItem sx={{border:'1px solid'}}/>
-                        <FormControl>
+                        <FormControl sx={{margin:'20px'}}>
                         <TextField
                         required
                         placeholder='Nombre y apellido'
