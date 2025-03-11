5c2fe80aa1782c7a4122236482a3e87c74ab990f
Maximo <maxipreneste@gmail.com>
2025-03-11 16:58:18 -0300

    fix (app):router out

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Footer from './Components/Footer'
 
@@ -7,14 +6,7 @@ import Footer from './Components/Footer'
 function App() {
   return (
     <>
-    
-    <Router>
-        <Routes>
-          <Route path="/*" element={<Home />} />
-          <Route path='/proyectos' element={<h1>Proyectos</h1>} />
-          <Route path="/proyectos/:nombre" element={<h1>pers</h1>} />
-        </Routes>
-    </Router>
+    <Home/>
     {/*Footer*/}
     <Footer/>
     
