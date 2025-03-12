de7fb58a3f2b691bdc24a3f2a0db2fbe73465207
Maximo <maxipreneste@gmail.com>
2025-03-12 12:15:10 -0300

    feat (footer): download CV

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,13 +7,13 @@ export default function Footer() {
     return (
         <>
             <footer>
-                <Divider sx={{border:'1px solid',margin:2}} />
+                <Divider sx={{border:'1px solid',margin:2}}  />
                 <Grid container spacing={2}>
-                    <Grid size={{ xs: 12, md: 6 }}>
+                    <Grid size={{ xs: 12, md: 6 }} >
                         <Typography variant='body2' align='center'>
                             2025. Maximo Preneste
                         </Typography>
-                        <Box sx={{display:'flex',justifyContent:'center'}}>
+                        <Box sx={{display:'flex',justifyContent:'center', margin:2}} >
                             <Social/>
                         </Box>
                     </Grid>
--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -1,5 +1,9 @@
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { Button } from '@mui/material';
+import DownloadIcon from '@mui/icons-material/Download';
+import {Box} from '@mui/material';
+import cv from '../assets/CV.pdf'
 export default function Social() {
     return(
         <>
@@ -7,7 +11,25 @@ export default function Social() {
         transform:'scale(1.25)',color:'#5687c0'} }}/></a>
         <a href="https://github.com/maxi-lab" target='_blanck'><GitHubIcon sx={{fontSize:50, color:'white', margin:3, "&:hover":{
         transform:'scale(1.25)',color:'#5687c0'}}}/></a>
-
+        <Box 
+        sx={{
+            display: "flex",
+            justifyContent: "center", // Centra horizontalmente
+            alignItems: "center", 
+            }}>
+        <Button variant='outlined'
+        component='a'
+        href={cv}
+        target='_blank'
+        download='Maximo_Preneste_CV.pdf'
+        size='small' sx={{
+            height:'50px',
+            borderRadius:'20%',
+            '&:hover':{
+                boxShadow:'5px 0 5px 5px  #4fde3c',
+            }
+        }}>CV <DownloadIcon/></Button>
+        </Box>
         </>
     )
 }
