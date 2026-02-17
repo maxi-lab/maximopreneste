6afc3340a2a2c3d97638f4be61bb62b1e14b3593
Maximo Preneste <maxipreneste@gmail.com>
2026-02-17 17:24:08 -0300

    feat(contenido): cambiar proyectos y subtitulos para pocisionamiento

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -5,43 +5,6 @@ import ski from '../assets/img/LOS PINOS.png'
 import is from '../assets/img/Eventhub.png'
 export const projects = [
     {
-        title:"Proyecto de Desarrollo de Software",
-        t:"Software Development Project",
-        description:"Una aplicacion web para un centro de ski para gestionar el estado de sus pistas y las atenciones a los heridos",
-        d:"A Ski Center web app to manage track state and the injured.",
-        img: ski,
-        link:' https://maxi-lab.github.io/Los-Pinos/',
-        github:'https://github.com/maxi-lab/LosPinos',
-        tecs:["C#",".NET","React","MUI","SQL Server"],
-        long:"Se implemento un backend con .NET y una base de datos SQL Server. El frontend fue desarrollado con React y MUI. Para la autenticacion se utilizo JWT. Aqui pude aplicar los principios SOLID en el backend. Actualmente, estoy trabajando en mejoras del frontend.",
-        l:"The backend is built on .NET. The database is SQL Server. The frontend was developed on React and MUI (for styles). The authentication is with JWT. Also, when I was disainging the backend I put into practice SOLID principals. At the moment, I'am warking on frontend's upgrades. "
-    },
-    {
-        title: "To Do App",
-        t:"To Do App",
-        description: "Una aplicacion basica de tareas",
-        d:"A task app",
-        img: ToDo,
-        link: "https://maxi-lab.github.io/Tareas-REACT/",
-        github:"https://github.com/maxi-lab/Tareas-REACT",
-        tecs:["React","TailwindCSS"],
-        long:"Una aplicacion basica de tareas, donde se pueden agregar, eliminar y marcar como completadas. En este proyecto, aprendí las bases de React y el uso basico de Vite."  , 
-        l:"A simple task application. A task can be added, deleted and markes as completed. In this project, I learned the React basics and the Vite use. "
-    },
-    {
-        title:"TP TyG web",
-        t:"Tecnology and Mangment of the Web project",
-        description: "Un TP de la materia Tecnologia y Gestion web ",
-        d:"A project of the subject Tecnology and Mangment of the Web",
-        img: TP1TYG,
-        link: "https://maxi-lab.github.io/TP1TYG/",
-        github: "https://github.com/maxi-lab/TP1TYG",
-        tecs:["HTML","CSS","JS"],
-        long:"Una pagina en la que se me solicita el consumo de dos APIs, solo con HTML, CSS y JavaScript",
-        l:"A simple web page that consumes two APIs, only with HTML, CSS y JavaScript"
-
-    },
-    {
         title:"Eventhub",
         t:"Eventhub",
         description: "Un sistema de gestion de eventos. Proyecto de Ingenieria de Software.",
@@ -53,6 +16,19 @@ export const projects = [
         long:"Este fue un proyecto grupal. En el que se desarrollaron ciertas funcionalidades, estas se testearon y se documentaron. Se dockerizo y se realizo CI/CD para desplegar la aplicacion en Render. ",
         l:"This project was in group. We developed some functionalities, tested them and documented them. The app was dockerized and CI/CD was made to deploy the app on Render."
     },
+    {
+        title:"Proyecto de Desarrollo de Software",
+        t:"Software Development Project",
+        description:"Una aplicacion web para un centro de ski para gestionar el estado de sus pistas y las atenciones a los heridos",
+        d:"A Ski Center web app to manage track state and the injured.",
+        img: ski,
+        link:' https://maxi-lab.github.io/Los-Pinos/',
+        github:'https://github.com/maxi-lab/LosPinos',
+        tecs:["C#",".NET","React","MUI","SQL Server"],
+        long:"Se implemento un backend con .NET y una base de datos SQL Server. El frontend fue desarrollado con React y MUI. Para la autenticacion se utilizo JWT. Aqui pude aplicar los principios SOLID en el backend. Actualmente, estoy trabajando en mejoras del frontend.",
+        l:"The backend is built on .NET. The database is SQL Server. The frontend was developed on React and MUI (for styles). The authentication is with JWT. Also, when I was disainging the backend I put into practice SOLID principals. At the moment, I'am warking on frontend's upgrades. "
+    },
+    
     {
         title:"Sistema de gestion de ventas y cobranzas",
         t:"Sell and collection managment system",
